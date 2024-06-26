7e4a29c9449bf4a88f835df3e1265df0351fcaff
mickey <michael.redaelli@gmail.com>
2024-06-26 03:57:02 +0100

    create dynamic request method

---
+++ request/dynamic_request.py
@@ -0,0 +1,27 @@
+import requests
+
+
+class Request:
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def dynamic_request(**kwargs) -> object:
+        method = kwargs.get('method', 'get').lower()
+        endpoint = kwargs.get('endpoint')
+        structure = kwargs.get('structure', 'json')
+
+        if not endpoint:
+            raise ValueError("Endpoint must be provided")
+        if not method:
+            raise ValueError(f"Invalid method: {method}")
+
+        kwargs.pop('method')
+        kwargs.pop('endpoint')
+        kwargs.pop('structure')
+
+        method = getattr(requests, method)
+        response = method(endpoint, **kwargs)
+
+        return getattr(response, structure)
